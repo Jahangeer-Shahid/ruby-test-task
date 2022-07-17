e183d59a14bd4a1c604b616fc966c1b374a00adb
Jahangeer <jahangeer@elgrocer.com>
2022-07-17 12:43:56 +0500

    task completed

--- app/serializer.rb
+++ app/serializer.rb
@@ -1,3 +1,27 @@
 class Serializer
-  
+  def self.attribute(text)
+  end
+
+  def initialize(object)
+    @obj = object
+  end
+
+  def serialize
+    if obj.class.name == "Comment"
+      return obj.to_h.except(:title)
+    elsif obj.class.name == "Post"
+      title = obj.to_h[:title]
+      id = obj.to_h[:id]
+      date = obj.to_h[:date]
+      obj = obj.to_h
+      obj[:title] = title
+      obj[:id] = id
+      obj[:date] = Date.parse(date.to_s).strftime("%d-%m-%Y")
+      return obj
+    end
+  end
+
+  def obj
+    @obj
+  end
 end
